eb4d6673346222865f0a03784e32c47661675c70
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-03-26 22:37:37 +0530

    Time: 16 ms (11.47%), Space: 22.1 MB (10.63%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -10,25 +10,21 @@
  * };
  */
 class Solution {
-     int maxDepth(TreeNode* root) {
+     int maxDepth(TreeNode* root, int& d) {
      if(root==NULL){
          return 0;
      }
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        int ans = max(left,right)+1;
-        return ans;
+        int l = maxDepth(root->left,d);
+        int r = maxDepth(root->right,d);
+        d=max(d,(l+r));
+        return  1+ max(l,r);
     }
 
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL){
-            return 0;
-        }
-        int op1 = diameterOfBinaryTree(root->left);
-         int op2 =diameterOfBinaryTree(root->right);
-        int op3 = maxDepth(root->left)  +maxDepth(root->right);
-        return max(op1,max(op2,op3));
+      int d=0;
+        maxDepth(root,d);
+        return d;
         
     }
 };
