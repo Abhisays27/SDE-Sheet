f2d7e7826437ba26d1b1bf41597481a35ecc8c76
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-04-03 13:14:56 +0530

    Time: 4 ms (40.11%), Space: 15.1 MB (5.67%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -4,11 +4,14 @@ class Solution {
             ans.push_back(output);
             return;
         }
+        //pick
+      
+        func(index+1,nums,output,ans);
+         output.push_back(nums[index]);
+         
          //not pick
         func(index+1,nums,output,ans);
-        //pick
-        output.push_back(nums[index]);
-        func(index+1,nums,output,ans);
+        
         
         
         
@@ -20,6 +23,7 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> output;
+        
         int index=0;
         func(index,nums,output,ans);
         return ans;
