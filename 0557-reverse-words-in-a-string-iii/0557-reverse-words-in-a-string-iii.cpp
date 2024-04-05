c9ad312a0875670f62e9ba3cd38a3afcc200bf6e
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-04-05 18:45:26 +0530

    Time: 17 ms (40.81%), Space: 16.8 MB (12.81%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n=s.size();
-        int l=0;
-        int r=0;
-
-        for(int i=0;i<n;i++){
-             if (s[i] == ' ') {
-                std::reverse(s.begin() + l, s.begin() + r);
-                l = i + 1; // Move l to the next position after space
-            } else {
-                r = i + 1; // Increment r if not a space
-            }
-          
-
+       stringstream ss(s);
+        string token = "";
+        string result ="";
+        
+        while(ss>>token){
+            reverse(token.begin(),token.end());
+            result+=token+ " ";
         }
-        //last
-          reverse(s.begin() + l, s.begin() + r);
-        return s;
+        
+        return result.substr(0,result.size()-1);
 
     }
 };
