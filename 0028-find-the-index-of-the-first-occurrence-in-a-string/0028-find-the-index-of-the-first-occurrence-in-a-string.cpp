4484b18f8acbeba6625765dd49de492ca0f1c439
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-04-09 00:09:31 +0530

    Time: 3 ms (42.17%), Space: 7.5 MB (57.02%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,40 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        char ch = needle[0];
-        int n = needle.size();
-        int i = 0;
-        while (i <= haystack.size() ) {
-            if (ch == haystack[i]) {
-                // Check if the substring matches
-                if (needle == haystack.substr(i, n)) {
-                    return i;
-                }
+        int m = haystack.size(), n = needle.size();
+        if (n == 0) return 0; // needle is empty
+        
+        // Constructing the prefix table for the needle string
+        vector<int> prefix(n);
+        int j = 0;
+        for (int i = 1; i < n; ++i) {
+            while (j > 0 && needle[i] != needle[j]) {
+                j = prefix[j - 1];
             }
-            i++;
+            if (needle[i] == needle[j]) {
+                prefix[i] = ++j;
+            }
         }
-        return -1;
+        
+        // Performing the KMP search
+        j = 0;
+        for (int i = 0; i < m; ++i) {
+            while (j > 0 && haystack[i] != needle[j]) {
+                j = prefix[j - 1];
+            }
+            if (haystack[i] == needle[j]) {
+                j++;
+            }
+            if (j == n) {
+                return i - n + 1; // Found the needle at index i
+            }
+        }
+        
+        return -1; // Needle not found
     }
 };
