9a9c96e2176a573932f28b311412a5a93db4bfbb
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-04-09 18:41:37 +0530

    Time: 0 ms (100.00%), Space: 13.5 MB (77.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,37 @@
 class Solution {
-    int getPivot(vector<int>& nums) {
-        int s = 0;
-        int e = nums.size() - 1;
-
-        while (s < e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] > nums[mid + 1]) {
-                return mid + 1;
-            } else if (nums[mid] > nums[s]) {
-                s = mid + 1;
-            } else {
-                e = mid;
-            }
-        }
-        return s;
-    }
-
-    int binarySearch(vector<int>& nums, int s, int e, int target) {
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        return -1;
-    }
-
 public:
-    int search(vector<int>& nums, int target) {
-        int pivot = getPivot(nums);
-        int n = nums.size() - 1;
-        if (target >= nums[pivot] && target <= nums[n]) {
-            return binarySearch(nums, pivot, n, target);
-        } else {
-            return binarySearch(nums, 0, pivot - 1, target);
+    int search(vector<int>& arr, int k) {
+        int n=arr.size();
+        int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return mid;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
         }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+
     }
 };
