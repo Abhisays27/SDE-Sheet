b01d186839b5b2a71615080ff3d0f7c9cb3754e3
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-04-11 16:28:25 +0530

    Time: 20 ms (79.21%), Space: 32.2 MB (37.40%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,21 +11,47 @@
  */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        
-    if(root) 
-            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
-            else if(key > root->val) root->right = deleteNode(root->right, key);       
-            else{
-                if(!root->left && !root->right) return NULL;          //No child condition
-                if (!root->left || !root->right)
-                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
-					                                                //Two child condition   
-                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
-                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
-                root->val = temp->val;                            //       root->val = temp->val;
-                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
-            }
+  
+TreeNode* minVal(TreeNode* root) {
+        while(root->left!=NULL){
+            root=root->left;
+        }
         return root;
-    }   
+    }
+
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return NULL;
+        if(root->val==key){
+            if(!root->left && !root->right) {
+                delete root;
+                return NULL;
+            }
+            if(!root->right && root->left){
+                TreeNode* left=root->left;
+                delete root;
+                return left;
+            }
+            if(root->right && !root->left){
+                TreeNode* right=root->right;
+                delete root;
+                return right;
+            }
+            if(root->left && root->right){
+                root->val=minVal(root->right)->val;
+                root->right=deleteNode(root->right,root->val);
+                return root;
+            }
+
+        }
+        else if(root->val>key){
+            root->left=deleteNode(root->left,key);
+            return root;
+        }
+        else{
+            root->right=deleteNode(root->right,key);
+            return root;
+        }
+        return NULL;
+    }
 };
