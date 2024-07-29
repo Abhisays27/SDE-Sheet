9e3cc5330ea240dd10380437bad780e8a008ae9c
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-07-30 00:08:02 +0530

    Time: 4 ms (38.82%), Space: 8.4 MB (70.03%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,32 +1,29 @@
 class Solution {
-    void func(int index,vector<int> nums,vector<int> output,vector<vector<int>>& ans){
-        if(index>=nums.size()){
+   void func(int index, vector<int>& nums, vector<int>& output, vector<vector<int>>& ans) {
+        if (index >= nums.size()) {
             ans.push_back(output);
             return;
         }
-        //pick
-      output.push_back(nums[index]);
-        func(index+1,nums,output,ans);
+        
+        // Pick the current element
+        output.push_back(nums[index]);
+        func(index + 1, nums, output, ans);
         output.pop_back();
-         
-         
-         //not pick
-        func(index+1,nums,output,ans);
         
+        // Skip duplicates
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) {
+            index++;
+        }
         
-        
-        
-
-       
-
+        // Not pick the current element
+        func(index + 1, nums, output, ans);
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> output;
-        
-        int index=0;
-        func(index,nums,output,ans);
+        sort(nums.begin(), nums.end()); // Sort the array to handle duplicates
+        func(0, nums, output, ans);
         return ans;
         
     }
