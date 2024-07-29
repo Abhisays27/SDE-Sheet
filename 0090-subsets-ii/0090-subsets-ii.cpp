75abacba9dd4abe45a231e5cc3c0d9a0b21d447c
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-07-30 00:12:07 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.38%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,30 @@
 class Solution {
- void func(int index,vector<int>& nums,vector<int>& ds,vector<vector<int>>& ans){
-     ans.push_back(ds);
-     
-     for(int i=index;i<nums.size();i++){
-         if(i!=index &&nums[i]==nums[i-1] ) continue;
-         ds.push_back(nums[i]);
-         func(i+1,nums,ds,ans);
-         ds.pop_back();
-     }
-     
+   
+      void func(int index, vector<int>& nums, vector<int>& output, vector<vector<int>>& ans) {
+        if (index >= nums.size()) {
+            ans.push_back(output);
+            return;
+        }
         
+        // Pick the current element
+        output.push_back(nums[index]);
+        func(index + 1, nums, output, ans);
+        output.pop_back();
+        
+        // Skip duplicates
+        while (index + 1 < nums.size() && nums[index] == nums[index + 1]) {
+            index++;
+        }
+        
+        // Not pick the current element
+        func(index + 1, nums, output, ans);
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        sort(nums.begin(),nums.end());
-        func(0,nums,ds,ans);
+        vector<int> output;
+        sort(nums.begin(), nums.end()); // Sort the array to handle duplicates
+        func(0, nums, output, ans);
         return ans;
         
     }
