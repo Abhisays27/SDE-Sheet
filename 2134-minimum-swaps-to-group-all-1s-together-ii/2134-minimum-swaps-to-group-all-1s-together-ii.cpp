338541344817f54b01cd4cbbcedd460438caa7cd
Abhishek Nautiyal <96642988+Abhisays27@users.noreply.github.com>
2024-08-02 11:34:18 +0530

    Time: 69 ms (53.24%), Space: 83.1 MB (57.56%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -3,38 +3,30 @@ public:
     int minSwaps(vector<int>& nums) {
         int n = nums.size();
         int swaps = INT_MAX;
-        int cnt =0;
-
+        int cnt = 0;
         int k = 0;
-        
-      
 
-        for(auto it : nums){
-            if(it==1) k++;
+        // Count total number of 1s in the array
+        for (auto it : nums) {
+            if (it == 1) k++;
         }
-          // If there are no 1s, no swaps needed
+
+        // If there are no 1s, no swaps needed
         if (k == 0) return 0;
 
-        int i=0;
-        int j=0;
-        while(j<n+k){
+        int i = 0, j = 0;
+        // Use a sliding window of size k to find the minimum swaps
+        while (j < n + k) {
+            if (nums[j % n] == 1) cnt++;
 
-            if(nums[j%n]==1) cnt++;
-
-            if(j-i+1<k) {
-                j++;
-            }
-            if(j-i+1==k){
-                swaps = min(swaps,k-cnt);
-                if(nums[i%n]==1) cnt--;
+            if (j - i + 1 == k) {
+                swaps = min(swaps, k - cnt);
+                if (nums[i % n] == 1) cnt--;
                 i++;
-                j++;
             }
-
-
-
-
+            j++;
         }
+
         return swaps;
     }
 };
